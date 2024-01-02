296524f92e54a892c8c73a4aa1645f004e45c99e
Arheee <justinewallet1@gmail.com>
2024-01-02 16:59:07 +0100

    Refactor test on  "Exercice 1 : Calcul de la moyenne" project :work in progress::refactor:

--- src/test/java/fr/arheee/demo/exercices/tp1/MoyenneTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp1/MoyenneTest.java
@@ -1,7 +1,11 @@
 package fr.arheee.demo.exercices.tp1;
 
+import jdk.internal.vm.annotation.DontInline;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +13,9 @@ class MoyenneTest {
 
     private Moyenne moyenne;
 
+    /**@
+     * creer une instance moyenne avant chaque test
+     */
     @BeforeEach
     public void setUp(){
         moyenne = new Moyenne();
@@ -16,8 +23,11 @@ class MoyenneTest {
 
     /**@
      * Renvoie la moyenne des notes
+     * @params int[] valeurs = tableau de notes
+     * Result la moyenne des notes
      */
     @Test
+    @DisplayName("Vérification du calcul de la moyenne")
     public void testVerifierMoyenne(){
         //Arrange
         int[] valeurs = {13, 20, 20};
@@ -27,5 +37,13 @@ class MoyenneTest {
 
         //Assert
         assertEquals(17, result);
+    }
+
+//    @DisplayName("Test Verification taille du tableau de notes")
+//    @Test
+//    public void testverifierTaille(int[] notes){
+//        IllegalArgumentException exception;
+//        assertThrows(IllegalArgumentException.class,
+//                () -> moyenne.moyenne(notes != 3));
     }
 }
