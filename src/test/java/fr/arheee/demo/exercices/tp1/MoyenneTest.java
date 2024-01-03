84e63f21a4b3838d249eeb425da197d0a168b195
Arheee <justinewallet1@gmail.com>
2024-01-03 09:52:26 +0100

    Tests on Exercice 1 : Calcul de la moyenne project :sparkles:

--- src/test/java/fr/arheee/demo/exercices/tp1/MoyenneTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp1/MoyenneTest.java
@@ -1,6 +1,5 @@
 package fr.arheee.demo.exercices.tp1;
 
-import jdk.internal.vm.annotation.DontInline;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,20 +10,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoyenneTest {
 
+    /**
+     * Attributs
+     */
     private Moyenne moyenne;
+   private  int[] plusQueTroisNotes = {13, 20, 12, 18};
+   private int[] seulementDeuxNotes = {13, 20};
+   private int[] noteInferieurZero = {-12, 12, 13};
+   private int[] noteSuperieurVingt = {34, 12, 13};
 
-    /**@
-     * creer une instance moyenne avant chaque test
+    /**
+     * Creer une instance moyenne avant chaque test
      */
     @BeforeEach
     public void setUp(){
         moyenne = new Moyenne();
     }
 
-    /**@
-     * Renvoie la moyenne des notes
+    /**
+     * Calcule la moyenne des notes
      * @params int[] valeurs = tableau de notes
-     * Result la moyenne des notes
+     * @Renvoie la moyenne des notes
      */
     @Test
     @DisplayName("Vérification du calcul de la moyenne")
@@ -39,11 +45,57 @@ class MoyenneTest {
         assertEquals(17, result);
     }
 
-//    @DisplayName("Test Verification taille du tableau de notes")
-//    @Test
-//    public void testverifierTaille(int[] notes){
-//        IllegalArgumentException exception;
-//        assertThrows(IllegalArgumentException.class,
-//                () -> moyenne.moyenne(notes != 3));
+    /**
+     * Vérifie que le carnet de notes contient 3 notes
+     * @Params int[] seulementDeuxNotes = tableau de deux notes
+     * @Renvoie Message d'exception : Le carnet de notes doit contenir 3 notes
+     */
+    @DisplayName("Test Verification taille du tableau de notes")
+    @Test
+    public void testSeulementDeuxNotes(){
+        IllegalArgumentException exception;
+        exception = assertThrows(
+                IllegalArgumentException.class ,
+                () ->
+                        moyenne.moyenne(seulementDeuxNotes));
+
+        assertEquals("Le carnet de notes doit contenir 3 notes", exception.getMessage());
+
+    }
+
+    /**
+     * Vérifie que le carnet de notes contiens 3 notes
+     * @Params int[] plusQueTroisNotes = tableau de plus de trois notes
+     * @Renvoie Message d'exception : Le carnet de notes doit contenir 3 notes
+     */
+    @DisplayName("Test Verification taille du tableau de notes")
+    @Test
+    public void testPlusQueTroisNotes(){
+        assertThrows(IllegalArgumentException.class,
+                () -> moyenne.moyenne(plusQueTroisNotes));
+    }
+
+    /**
+     * Vérifie que les notes sont comprises entre 0 et 20
+     * @Params int[] noteInferieurZero = Tableau de notes avec une notes en dessous de zéro
+     * @Renvoie Les notes doivent être comprises entre 0 et 20
+     */
+    @DisplayName("Test Verification Si une note est en dessous de zéro")
+    @Test
+    public void testNoteInferieurZero(){
+        assertThrows(IllegalArgumentException.class,
+                () -> moyenne.moyenne(noteInferieurZero));
+    }
+
+    /**
+     * Vérifie que les notes sont comprises entre 0 et 20
+     * @Params int[] noteSuperieurVingt = Tableau de notes avec une notes au dessus de vingt
+     * @Renvoie Les notes doivent être comprises entre 0 et 20
+     */
+    @DisplayName("Test Verification Si une note est au dessus de vingt")
+    @Test
+    public void testNoteSuperieurVingt(){
+        assertThrows(IllegalArgumentException.class,
+                () -> moyenne.moyenne(noteSuperieurVingt));
     }
 }
