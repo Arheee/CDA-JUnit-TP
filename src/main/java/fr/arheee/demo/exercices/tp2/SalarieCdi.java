b51f8aa51a108124afb66a932522231c67415038
Arheee <justinewallet1@gmail.com>
2024-01-04 13:11:59 +0100

    TP2 - SalarieCdi Class :sparkles: :memo:

---
+++ src/main/java/fr/arheee/demo/exercices/tp2/SalarieCdi.java
@@ -0,0 +1,15 @@
+package fr.arheee.demo.exercices.tp2;
+
+
+public class SalarieCdi extends Salarie {
+
+    /**
+     *Constructeur
+     * @param salaire Salaire associé au salarié en CDI
+     * @param identite Identité du salarié en CDI
+     */
+    public SalarieCdi(Salaire salaire, Identite identite) {
+            super(salaire, identite);
+        }
+}
+
