403d7b546bacc3ec634590d53c9d27a87e4e47d5
Arheee <justinewallet1@gmail.com>
2024-01-05 16:31:07 +0100

    feat : Thermometre :sparkles:

--- src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
@@ -1,4 +1,5 @@
 package fr.arheee.demo.exercices.tp3;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,8 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ThermometreTest {
 
+
     @ParameterizedTest
     @MethodSource("streamTemperatureThermometre")
+    @DisplayName("Test thermomètre qui permet d'indiquer la température la plus proche de 0")
     public void test_stream_return_temperature_positive_if_negative_equal(int[] temperatures, int expected ) {
         int result = Thermometre.findClosestTemperature(temperatures);
         assertEquals(expected, result);
@@ -25,6 +28,7 @@ public class ThermometreTest {
     }
 
     @Test
+    @DisplayName("Test plus de 10000 mesures, on lève une exception ")
     public void testTooManyMeasurements() {
         int[] temperatures = new int[10001];
         assertThrows(IllegalArgumentException.class, () -> Thermometre.findClosestTemperature(temperatures));
