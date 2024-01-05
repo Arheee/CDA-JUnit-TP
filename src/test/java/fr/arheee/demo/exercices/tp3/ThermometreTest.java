c9b0830f022f2b752a8c3491bf9f933f208dc27e
Arheee <justinewallet1@gmail.com>
2024-01-05 16:19:48 +0100

    Refactor test : ThermometreTest return positive number when 2 equal :white_check_mark:

--- src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
@@ -1,28 +1,36 @@
 package fr.arheee.demo.exercices.tp3;
 
+import fr.arheee.demo.exercices.tp1.Doublon;
+import fr.arheee.demo.exercices.tp2.Adresse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
 
 public class ThermometreTest {
-    @Test
-    public void test_thermometre_return_0() {
-        int[] temperature = new int[]{};
-        int result = Thermometre.findClosestTemperature(temperature);
-        assertEquals(0, result);
+
+    @ParameterizedTest
+    @MethodSource("streamTemperatureThermometre")
+    public void test_stream_return_temperature_positive_if_negative_equal(int[] temperatures, int expected ) {
+        int result = Thermometre.findClosestTemperature(temperatures);
+        assertEquals(expected, result);
     }
 
-    @Test
-    public void test_thermometre_return_1() {
-        int[] temperature = new int[]{1, 2, 3};
-        int result = Thermometre.findClosestTemperature(temperature);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void test_thermometre_return_positive_temperature_if_negative_temperature_equal(){
-        int[] temperature = new int[]{-2, -8, -4, 2};
-        int result = Thermometre.findClosestTemperature(temperature);
-        assertEquals(2, result);
+    private static Stream<Arguments> streamTemperatureThermometre() {
+        return Stream.of(
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{1, 2, 3}, 1),
+                Arguments.of(new int[]{-2, -8, -4, 2}, 2),
+                Arguments.of(new int[]{-2, -8, 4, 5}, -2)
+        );
     }
 }
