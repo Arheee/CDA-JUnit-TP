eba1d625fb97d4d26b3f26bdb90726dfcc71f2c6
Arheee <justinewallet1@gmail.com>
2024-01-05 09:40:48 +0100

    TP2 - Refactor Test on IdentiteTest with mock :refactor:

--- src/test/java/fr/arheee/demo/exercices/tp2/IdentiteTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp2/IdentiteTest.java
@@ -5,40 +5,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class IdentiteTest {
 
     private Identite identite;
-    private Adresse adresse;
 
+    @Mock
+    private Adresse adresseMock;
     @BeforeEach
     public void setUp(){
-        adresse = new Adresse("123"," rue jean du pont","75001","Paris","France");
-        identite = new Identite("Dupont","Jean", adresse);
+        identite = new Identite("Dupont","Jean", adresseMock);
     }
 
 
-    /**
-     * Test paramétrés pour changer d'adresse
-     * @param voie le numéro
-     * @param rue le pays .. non j'rigole cest la rue
-     * @param cp le code postal
-     * @param ville la ville
-     * @param pays le pays
-     */
-    @ParameterizedTest
+    @Test
     @DisplayName("Test paramétré de changement d'adresse ")
-    @CsvSource({
-            "456, rue jean du grand pont, 75001, Paris, France",
-            "789, avenue des duponts, 75008, Paris, France"
-    })
-    void testChangementAdresse(String voie, String rue, String cp, String ville, String pays) {
-        Adresse result = new Adresse(voie, rue, cp, ville, pays);
-        identite.setAdresse(result);
-
-        assertEquals(result, identite.getAdresse());
+    void testChangementAdresse() {
+        Adresse nouvelleAdresseMock = mock(Adresse.class);
+        identite.setAdresse(nouvelleAdresseMock);
+        assertEquals(nouvelleAdresseMock, identite.getAdresse());
     }
 
 }
