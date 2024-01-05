e20116efca99e1b06c2b359af3ba68e9b7514e4a
Arheee <justinewallet1@gmail.com>
2024-01-05 15:10:13 +0100

    test : ThermometreTest for return 1 :test_tube:

--- src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp3/ThermometreTest.java
@@ -10,4 +10,11 @@ public class ThermometreTest {
         int result = Thermometre.findClosestTemperature(0);
         assertEquals(0, result);
     }
+
+    @Test
+    public void test_thermometre_return_1() {
+        int[] temperatures = new int[]{1, 2, 3};
+        int result = Thermometre.findClosestTemperature(1);
+        assertEquals(1, result);
+    }
 }
