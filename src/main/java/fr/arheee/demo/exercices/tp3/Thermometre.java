8644dbfecb41f7c376bdc5239b587386dc3b1f21
Arheee <justinewallet1@gmail.com>
2024-01-05 15:54:13 +0100

    test : ThermometreTest return positive number when 2 equal :white_check_mark:

--- src/main/java/fr/arheee/demo/exercices/tp3/Thermometre.java
+++ src/main/java/fr/arheee/demo/exercices/tp3/Thermometre.java
@@ -2,11 +2,19 @@ package fr.arheee.demo.exercices.tp3;
 
 public class Thermometre {
 
-        public static int findClosestTemperature(int[] temperature) {
-            if (temperature.length == 0) {
+        public static int findClosestTemperature(int[] temperatures) {
+            if (temperatures.length == 0) {
                 return 0;
             }
-            return temperature[0];
+            int closestTemperature = temperatures[0];
+            for (int temperature : temperatures) {
+                if (Math.abs(temperature) < Math.abs(closestTemperature) ||
+                        (Math.abs(temperature) == Math.abs(closestTemperature) && temperature > 0)) {
+                    closestTemperature = temperature;
+                }
+            }
+
+            return closestTemperature;
         }
 
 }
