1e5cb09d28688276c8428fd5f52a2c4ec588eb94
Arheee <justinewallet1@gmail.com>
2024-01-06 18:32:15 +0100

    test : Stack push :white_check_mark:

---
+++ src/main/java/fr/arheee/demo/exercices/tp3/Stack.java
@@ -0,0 +1,18 @@
+package fr.arheee.demo.exercices.tp3;
+
+public class Stack {
+    private int[] elements;
+
+
+    public Stack() {
+        this.elements = new int[10];
+    }
+
+    public void push(int element) {
+         elements = new int[1];
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
