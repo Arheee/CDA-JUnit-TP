113dd0c61f01173faf6d92e71499e8a4fb328e4b
Arheee <justinewallet1@gmail.com>
2024-01-06 18:50:09 +0100

    test : Stack peek :test_tube:

--- src/test/java/fr/arheee/demo/exercices/tp3/StackTest.java
+++ src/test/java/fr/arheee/demo/exercices/tp3/StackTest.java
@@ -3,8 +3,7 @@ package fr.arheee.demo.exercices.tp3;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StackTest {
     private Stack stack;
@@ -19,10 +18,20 @@ public class StackTest {
     }
 
     @Test
-    public void testPop() {
+    public void test_pop() {
         stack.push(1);
         stack.pop();
         boolean result = stack.isEmpty();
         assertTrue(result);
+    }
+
+    @Test
+    public void test_peek() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        int result = stack.peek();
+        assertEquals(3, result);
+        assertFalse(stack.isEmpty());
     }
 }
